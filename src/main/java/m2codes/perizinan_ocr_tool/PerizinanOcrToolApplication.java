57b13c89c4f140ded9413963fd01dd3e678c6c78
Marij Mokoginta <marijmokoginta21@gmail.com>
2024-08-30 21:50:40 +0800

    project initialized

---
+++ src/main/java/m2codes/perizinan_ocr_tool/PerizinanOcrToolApplication.java
@@ -0,0 +1,13 @@
+package m2codes.perizinan_ocr_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerizinanOcrToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerizinanOcrToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/m2codes/perizinan_ocr_tool/PerizinanOcrToolApplicationTests.java
@@ -0,0 +1,13 @@
+package m2codes.perizinan_ocr_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerizinanOcrToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
