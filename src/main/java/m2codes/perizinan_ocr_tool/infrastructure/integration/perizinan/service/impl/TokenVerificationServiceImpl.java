456ffcd7d64114ff7e883c6a2e320117e4ab7220
Marij Mokoginta <marijmokoginta21@gmail.com>
2024-09-22 20:50:43 +0800

    create endpoint and service for token verification

---
+++ src/main/java/m2codes/perizinan_ocr_tool/infrastructure/integration/perizinan/service/impl/TokenVerificationServiceImpl.java
@@ -0,0 +1,31 @@
+package m2codes.perizinan_ocr_tool.infrastructure.integration.perizinan.service.impl;
+
+import m2codes.perizinan_ocr_tool.infrastructure.integration.perizinan.dto.UserResponse;
+import m2codes.perizinan_ocr_tool.infrastructure.integration.perizinan.endpoint.TokenVerificationEndpoint;
+import m2codes.perizinan_ocr_tool.infrastructure.integration.perizinan.service.TokenVerificationService;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class TokenVerificationServiceImpl implements TokenVerificationService {
+
+    private final TokenVerificationEndpoint tokenVerificationEndpoint;
+
+    public TokenVerificationServiceImpl(
+            TokenVerificationEndpoint tokenVerificationEndpoint
+    ) {
+        this.tokenVerificationEndpoint = tokenVerificationEndpoint;
+    }
+
+    @Async
+    @Override
+    public CompletableFuture<UserResponse> getCurrentUser(String token) {
+        UserResponse response = Optional.ofNullable(tokenVerificationEndpoint.getCurrentUser(token))
+                .orElseThrow();
+        return CompletableFuture.completedFuture(response);
+    }
+
+}
