cab685ff022de1aefac7d8fa57d05b316a07e2c0
Marij Mokoginta <marijmokoginta21@gmail.com>
2024-10-14 01:36:21 +0800

    update

--- src/main/java/m2codes/perizinan_ocr_tool/application/config/OcrConfig.java
+++ src/main/java/m2codes/perizinan_ocr_tool/application/config/OcrConfig.java
@@ -1,30 +1,42 @@
 package m2codes.perizinan_ocr_tool.application.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import net.sourceforge.tess4j.Tesseract;
-import org.springframework.core.io.Resource;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 /**
  *
  * @author marij_mokoginta
  */
+@Slf4j
 @Configuration
 public class OcrConfig {
-
-    @Value("classpath:static/tessdata")
-    private Resource tessdataDirectory;
 
     @Bean
     public Tesseract tesseract() throws IOException {
         Tesseract tesseract = new Tesseract();
 
-        String tessdataPath = tessdataDirectory.getFile().getAbsolutePath();
-        tesseract.setDatapath(tessdataPath);
+        Path tessdataPath = Files.createTempDirectory("tessdata");
+        ClassPathResource resource = new ClassPathResource("static/tessdata");
+        Files.walk(resource.getFile().toPath())
+                        .forEach(source -> {
+                            try {
+                                Path destination = tessdataPath.resolve(resource.getFile().toPath().relativize(source));
+                                Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+                            } catch (IOException e) {
+                                log.error(e.getMessage());
+                            }
+                        });
+
+        tesseract.setDatapath(tessdataPath.toString());
         tesseract.setLanguage("ind");
 
         return tesseract;
