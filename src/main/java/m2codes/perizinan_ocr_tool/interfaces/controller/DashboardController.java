b861998b9948a9e3bd39cc0539371458e4baff76
Marij Mokoginta <marijmokoginta21@gmail.com>
2025-03-04 04:35:55 +0800

    update profile page

--- src/main/java/m2codes/perizinan_ocr_tool/interfaces/controller/DashboardController.java
+++ src/main/java/m2codes/perizinan_ocr_tool/interfaces/controller/DashboardController.java
@@ -1,16 +1,25 @@
 package m2codes.perizinan_ocr_tool.interfaces.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import m2codes.perizinan_ocr_tool.domain.model.User;
+import m2codes.perizinan_ocr_tool.domain.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("dashboard")
+@RequiredArgsConstructor
+@Slf4j
 public class DashboardController {
+
+    private final UserService userService;
 
     @GetMapping(path = "")
     public String index(Model model) {
@@ -37,6 +46,14 @@ public class DashboardController {
     @GetMapping(path = "/docs")
     public String docs() {
         return "dashboard/docs";
+    }
+
+    @GetMapping(path = "/profile")
+    public String profile(Model model, Principal principal) {
+        log.info("user principal: {}", principal.getName());
+        User user = userService.findByUsername(principal.getName()).orElseThrow();
+        model.addAttribute("user", user);
+        return "dashboard/profile";
     }
 
 }
