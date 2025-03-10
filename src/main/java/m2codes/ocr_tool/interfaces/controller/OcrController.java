f124ec8e6ac4e3053128ba5c8bf5c979047479a9
Marij Mokoginta <marijmokoginta21@gmail.com>
2025-03-11 00:19:31 +0800

    update validation error response

--- src/main/java/m2codes/ocr_tool/interfaces/controller/OcrController.java
+++ src/main/java/m2codes/ocr_tool/interfaces/controller/OcrController.java
@@ -16,9 +16,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/ocr")
@@ -64,9 +67,11 @@ public class OcrController {
         }
 
         if (result.hasErrors()) {
-            List<String> errors = result.getAllErrors().stream()
-                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                    .toList();
+            Map<String, List<String>> errors = result.getAllErrors().stream()
+                    .collect(Collectors.groupingBy(
+                            error -> (error instanceof FieldError) ? ((FieldError) error).getField() : "global",
+                            Collectors.mapping(DefaultMessageSourceResolvable::getDefaultMessage, Collectors.toList())
+                    ));
 
             return ResponseEntity.badRequest().body(ApiResponse.builder()
                     .success(false)
